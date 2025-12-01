b0638089ebff8899ccd91be0037ab7fe138b8691
maad328 <maad78150@gmail.com>
2025-12-01 19:09:48 +0500

    added cors to connect

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from .router import signup
 from .router import profile
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow req from any website like frontend
+    allow_credentials=True,  # allow sending cookies,tokens
+    allow_methods=["*"],  # allow methods get,post,del
+    # allow frontend to send any custom header like tokens
+    allow_headers=["*"],
+)
 # defining routes
 app.include_router(signup.router)
 app.include_router(profile.router)
